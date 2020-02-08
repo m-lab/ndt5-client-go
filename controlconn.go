cf7dbbc02acb3c9b820070b2035a29d21337bd98
Simone Basso <bassosimone@gmail.com>
2020-02-08 19:21:26 +0100

    controlconn.go: rename pointer to struct

--- controlconn.go
+++ controlconn.go
@@ -23,31 +23,33 @@ type controlconn struct {
 	conn net.Conn
 }
 
-func (bcc *controlconn) SetDeadline(deadline time.Time) error {
-	return bcc.conn.SetDeadline(deadline)
+func (cc *controlconn) SetDeadline(deadline time.Time) error {
+	return cc.conn.SetDeadline(deadline)
 }
 
-func (bcc *controlconn) ReadMessage() (mtype uint8, data []byte, err error) {
+const maxMessageSize = math.MaxUint16 + 3
+
+func (cc *controlconn) ReadMessage() (mtype uint8, data []byte, err error) {
 	// <type: uint8> <length: uint16> <message: [0..65536]byte>
 	b := make([]byte, 1)
-	if err = bcc.Readn(b); err != nil {
+	if err = cc.Readn(b); err != nil {
 		return
 	}
 	mtype = b[0]
 	b = make([]byte, 2)
-	if err = bcc.Readn(b); err != nil {
+	if err = cc.Readn(b); err != nil {
 		return
 	}
 	length := binary.BigEndian.Uint16(b)
 	data = make([]byte, length)
-	err = bcc.Readn(data)
+	err = cc.Readn(data)
 	return
 }
 
-func (bcc *controlconn) WriteMessage(mtype uint8, data []byte) error {
+func (cc *controlconn) WriteMessage(mtype uint8, data []byte) error {
 	// <type: uint8> <length: uint16> <message: [0..65536]byte>
 	b := []byte{mtype}
-	if _, err := bcc.conn.Write(b); err != nil {
+	if _, err := cc.conn.Write(b); err != nil {
 		return err
 	}
 	if len(data) > math.MaxUint16 {
@@ -55,19 +57,19 @@ func (bcc *controlconn) WriteMessage(mtype uint8, data []byte) error {
 	}
 	b = make([]byte, 2)
 	binary.BigEndian.PutUint16(b, uint16(len(data)))
-	if _, err := bcc.conn.Write(b); err != nil {
+	if _, err := cc.conn.Write(b); err != nil {
 		return err
 	}
-	_, err := bcc.conn.Write(data)
+	_, err := cc.conn.Write(data)
 	return err
 }
 
-func (bcc *controlconn) Readn(data []byte) error {
+func (cc *controlconn) Readn(data []byte) error {
 	// We don't care too much about performance when reading
 	// control messages, hence this simple implementation
 	for off := 0; off < len(data); {
 		curr := make([]byte, 1)
-		if _, err := bcc.conn.Read(curr); err != nil {
+		if _, err := cc.conn.Read(curr); err != nil {
 			return err
 		}
 		data[off] = curr[0]
@@ -76,6 +78,6 @@ func (bcc *controlconn) Readn(data []byte) error {
 	return nil
 }
 
-func (bcc *controlconn) Close() error {
-	return bcc.conn.Close()
+func (cc *controlconn) Close() error {
+	return cc.conn.Close()
 }
